144702ba72c2ffcfa05d6fc0b9f0292a08184e6b
Leonel Ortega Fabian <leo@MacBook-Air-de-Leonel.local>
2018-01-04 13:14:53 -0600

    first commit

---
+++ basic_app/views.py
@@ -0,0 +1,39 @@
+from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views.generic import (View, TemplateView, ListView, DetailView,
+                                    CreateView, UpdateView, DeleteView)
+from . import models
+
+class IndexView(TemplateView):
+    template_name = 'index.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['injectme'] = 'contect injected'
+        return context
+
+class SchoolListView(ListView):
+    # because SchoolListView inherits from ListView, takes model, lowercase, and apply model_list
+    # wich is returned and injected to context dictionary (school_list)
+    # however we can override context_object_name
+    context_object_name = 'schools'
+    model = models.School
+
+class SchoolDetailView(DetailView):
+    # in this case the model in lowercase is returned and injected to context dictionary (school)
+    # but again we can override context_object_name to be more clear
+    context_object_name = 'school_detail'
+    model = models.School
+    template_name = 'basic_app/school_detail.html'
+
+class SchoolCreateView(CreateView):
+    fields = ('name', 'principal', 'location')
+    model = models.School
+
+class SchoolUpdateView(UpdateView):
+    fields = ('name', 'principal')
+    model = models.School
+
+class SchoolDeleteView(DeleteView):
+    model = models.School
+    success_url = reverse_lazy('basic_app:list')
